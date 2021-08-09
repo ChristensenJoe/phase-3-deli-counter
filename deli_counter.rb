43797a11169fb55c5423d64e2e9b98134087fe08
Joe Christensen <joechristensenwork@gmail.com>
2021-08-09 12:28:57 -0700

    commit tree ee40251eabd736e8ce0dc6595f7c2842e5cbd2a1

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+katz_deli = []
+
+def line(queue)
+  customers = ""
+  queue.each do |name|
+    if name == queue.last
+      customers += "#{queue.index(name) + 1}. #{name}"
+    else
+      customers += "#{queue.index(name) + 1}. #{name} "
+    end
+  end
+  if queue.length != 0
+    puts "The line is currently: #{customers}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(queue, name)
+  queue.push(name)
+  puts "Welcome, #{name}. You are number #{queue.length} in line."
+end
+
+def now_serving(queue)
+  if queue.length != 0
+    puts "Currently serving #{queue.shift}."
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
